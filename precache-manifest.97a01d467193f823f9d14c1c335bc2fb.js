377ad6df6ae33d5934973c5fab8cdd386c105947
saurabh_singh <imsaurabhsingh007@gmail.com>
2020-05-18 17:04:33 +0530

    Updates

---
+++ precache-manifest.97a01d467193f823f9d14c1c335bc2fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9a4d2d5745d1a26255809dc3390197",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "8ea1241982370edecbfe",
+    "url": "/react-todo-app/static/css/main.3855aa71.chunk.css"
+  },
+  {
+    "revision": "f3cdcaabcaac243879e7",
+    "url": "/react-todo-app/static/js/2.b529977d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.b529977d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea1241982370edecbfe",
+    "url": "/react-todo-app/static/js/main.c1aa2a1f.chunk.js"
+  },
+  {
+    "revision": "8b95f8163613479170b2",
+    "url": "/react-todo-app/static/js/runtime-main.e53e5e31.js"
+  }
+]);
